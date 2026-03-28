e3a8506fd8293f024b95af825a51026a6f1ff7fc
morganwang010 <morganwang010@sina.com>
2026-03-28 22:49:08 +0800

    refactor(router): 移除登录页面的LicenseProvider包装
    ci: 更新CI/CD脚本中的进程终止命令

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -62,9 +62,9 @@ const router = createBrowserRouter([
     path: '/login',
     element: (
       <AuthProvider>
-        <LicenseProvider>
+        {/* <LicenseProvider> */}
           <LoginPage />
-        </LicenseProvider>
+        {/* </LicenseProvider> */}
       </AuthProvider>
     ),
   },
